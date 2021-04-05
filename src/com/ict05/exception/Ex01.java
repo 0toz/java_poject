225a9417e912dffa6aebc92268d1ffb59adaf072
0toz <0toz@kakao.com>
2021-04-05 18:14:00 +0900

    123

---
+++ src/com/ict05/exception/Ex01.java
@@ -0,0 +1,16 @@
+package com.ict05.exception;
+
+public class Ex01 {
+//	제네릭과 컬렉션 (자료구조)
+//	제네릭과 컬렉션이 어떤 객체들로 이루어졌는지를 표시하는 객체 타입을 말한다.
+//  컬렉션 : 객체들을 모아서 관리하는 인터페이스를 구현한것.
+// API : <T> 객체 타입을 의미함. <E> 요소를 의미함.( 컬렉션 안에 존재하는 객체 하나를 뜻한다)
+//	Map형식에서는 <K, V> 키 와 값.
+//	Key 와 Values가 1대1 대응ㅇ key를 호출하면 Value가 나온다.
+//	형식 : 컬렉션<제네릭>
+// 최상위 컬렉션은 Collection<E>, Map<K,V>
+//	Collection<E>를 상속받은 인터페이스 : Set<E> , List<E>, Queue<E>
+//	Collection의 주요 메소드
+	
+	
+}

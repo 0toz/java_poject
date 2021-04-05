3b65f16ed68402b2bd2265e3bf69758a41dc28aa
0toz <0toz@kakao.com>
2021-04-05 13:22:23 +0900

    ss

--- src/com/ict01/grammar/Grammer1Ex01.java
+++ src/com/ict01/grammar/Grammer1Ex01.java
@@ -2,5 +2,9 @@ package com.ict01.grammar;
 class Grammer1Ex01{
 	public static void main(String[] args){
 		System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+		System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+		System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+		System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+		System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
 	}
 }

dc72a7aba00b521f21917a80dd61a0a15dd50dda
0toz <0toz@kakao.com>
2021-04-05 15:48:57 +0900

    ㅇㅇㅇ

--- src/com/ict04/exception/Ex01.java
+++ src/com/ict04/exception/Ex01.java
@@ -2,6 +2,7 @@ package com.ict04.exception;
 
 public class Ex01 {
 	public static void main(String[] args) {
-			System.out.println("이제 제대로 나오기를");
+			System.out.println("이제 제대로 나오기를ㅇㅇㅇ");
+			///이거맞나 할정도록.
 	}
 }
